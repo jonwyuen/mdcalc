098328d7a4922a0233cb78a9a7e51ea20b9980c9
Jonathan Yuen <jonwyuen@gmail.com>
2018-01-27 19:59:20 -0800

    making it one form

--- js/index.js
+++ js/index.js
@@ -1,26 +1,22 @@
 const anionGap = (na, cl, bicarb) => na - (cl + bicarb);
 const serumOsmality = (na, k, bun, glucose) => +(2 * (na + k) + (bun/2.8) + (glucose/18)).toPrecision(5);
 
-let anionGapForm = document.querySelector('#ag-form');
-let serumOsmalityForm = document.querySelector('#so-form');
+let form = document.querySelector('form');
 
-anionGapForm.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
-  let na = document.querySelector('#ag-na').value;
-  let cl = document.querySelector('#ag-cl').value;
-  let bicarb = document.querySelector('#ag-bicarb').value;
-  let result = anionGap(+na, +cl, +bicarb);
-  document.querySelector('#ag-result').innerText = result;
-  anionGapForm.reset();
-});
+  let agNa = document.querySelector('#ag-na').value;
+  let agCl = document.querySelector('#ag-cl').value;
+  let agBicarb = document.querySelector('#ag-bicarb').value;
+  let agResult = anionGap(+agNa, +agCl, +agBicarb);
 
-serumOsmalityForm.addEventListener('submit', e => {
-  e.preventDefault();
-  let na = document.querySelector('#so-na').value;
-  let k = document.querySelector('#so-k').value;
-  let bun = document.querySelector('#so-bun').value;
-  let glucose = document.querySelector('#so-glucose').value;
-  let result = serumOsmality(+na, +k, +bun, +glucose);
-  document.querySelector('#so-result').innerText = result;
-  serumOsmalityForm.reset();
+  let soNa = document.querySelector('#so-na').value;
+  let soK = document.querySelector('#so-k').value;
+  let soBun = document.querySelector('#so-bun').value;
+  let soGlucose = document.querySelector('#so-glucose').value;
+  let soResult = serumOsmality(+soNa, +soK, +soBun, +soGlucose);
+
+  document.querySelector('#ag-result').innerHTML = `<strong>${agResult}</strong>`;
+  document.querySelector('#so-result').innerHTML = `<strong>${soResult}</strong>`;
+  form.reset();
 });
